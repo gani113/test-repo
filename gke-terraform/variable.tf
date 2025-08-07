c5b49e5911bd243d2757f3bdab6f84db2838a24b
gani113 <saidgani113@gmail.com>
2025-08-07 15:48:36 +0530

    Update variable.tf

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -1,19 +1,19 @@
 variable "project" {
   description = "this is gcp project-id"
   type        = string
-  default     = "rock-loop-449918-b0"
+  default     = "binge-projects"
 }
 
 variable "region" {
   description = "this is gcp region"
   type        = string
-  default     = "africa-south1"
+  default     = "us-central1"
 }
 
 variable "zone" {
   description = "this is gcp zone"
   type        = string
-  default     = "africa-south1-a"
+  default     = "us-central1-c"
 }
 
 variable "K8s_version" {
